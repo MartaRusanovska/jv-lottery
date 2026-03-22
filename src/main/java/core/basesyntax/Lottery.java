980b458f9c6bec175ec89af784f1a38bb2758dbf
MartaRusanovska <kotykmarta@gmail.com>
2026-03-22 02:05:03 +0100

    fixing for lottery task

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,8 +3,13 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    Random random = new Random();
     private static final int CONSTANT = 101;
+    private Random random = new Random();
+
+    public Random getRandom() {
+        return random;
+    }
+
     public Ball getRandomBall() {
         int value = random.nextInt(CONSTANT);
         ColorSupplier colorSupplier = new ColorSupplier();
